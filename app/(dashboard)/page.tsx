8fe5183d60485999788f4df74dabc090ea235886
horidianx69 <ayush.shuklaps@gmail.com>
2024-08-29 17:47:33 +0530

    initial commit

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <p>
+      this is an authenticated route.
+    </p>
+  )
+}
+
